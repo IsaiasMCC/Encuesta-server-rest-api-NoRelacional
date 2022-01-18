42ebdff840903d8a90092859a6c7f30939d7710d
IsaiasMCC <isaisvhsyw@gmail.com>
2022-01-18 00:38:57 -0400

    add resultados de encuesta

--- src/controllers/EncuestaAplicadaController.js
+++ src/controllers/EncuestaAplicadaController.js
@@ -1,36 +1,10 @@
 const { EncuestaAplicada } = require('../models/EncuestaAplicada');
 const { Encuesta } = require('../models/Encuesta');
 const {OptionRespuesta } = require('../models/OptionRespuesta');
+const { Pregunta } = require('../models/Pregunta')
 const mongoose = require('mongoose');
 
-// const postEncuestaAplicada = async (req, res ) => {
-//     const { id_encuesta, answers} = req.body;
-    
-//     const validarId =  mongoose.isValidObjectId(id_encuesta);
-//     if (!validarId){
-//         return res.status(401).json({ success: false, message: 'Id no valido'});
-//     }
-//     const encuesta = await Encuesta.findById(id_encuesta);
-//     if (!encuesta){
-//         return res.status(401).json({ success: false, message: 'La encuesta no existe'});
-//     }
-//     const id_option_respuesta = new OptionRespuesta({value: 'Ninguno'});
-//     await id_option_respuesta.save();
-//     const answers1 = answers.map( (item) => {
-//         const container = item;
-       
-//         if (!container.id_option_respuestas){
-//             container.id_option_respuestas = [id_option_respuesta.id];
-//         }
-//         return container
-//     });
-//     const encuestaaplicada = new EncuestaAplicada({ id_encuesta, answers: answers1 });
-//     if (!encuestaaplicada){
-//         return res.status(401).json({ success: false, message: 'Encuesta Aplicada no creada error'});
-//     }
-//     await encuestaaplicada.save();
-//     return res.json({ success: true, message: 'Encuesta Aplicada agregada correctamente'});
-// }
+
 const postEncuestaAplicada = async (req, res) => {
     const { answers } = req.body;
         const validarId =  mongoose.isValidObjectId(answers.id_encuesta);
@@ -50,6 +24,7 @@ const postEncuestaAplicada = async (req, res) => {
     return res.status(200).json({ success: true, message: 'aplicacion encuesta creada!' });
 } 
 
+
 const getEncuestasAplicadas = async (req, res) => {
     const encuestasaplicadas = await EncuestaAplicada.find().populate([ { path: 'answers.id_question', model: 'Pregunta', select: 'name'}, { path: 'answers.id_option_respuestas', model: 'OptionRespuesta', select: 'value'}]);
     if (!encuestasaplicadas){
@@ -58,8 +33,64 @@ const getEncuestasAplicadas = async (req, res) => {
     res.json({ success: true, encuestasaplicadas});
 }
 
+const getResultadosPr = async (req, res) => {
+    const { id_encuesta, id_pregunta} = req.body;
+    
+    const validarId =  mongoose.isValidObjectId(id_encuesta);
+    const validarId2 = mongoose.isValidObjectId(id_pregunta)
+    if (!validarId){
+        return res.status(401).json({ success: false, message: 'Id no valido'});
+    }
+    if (!validarId2){
+        return res.status(401).json({ success: false, message: 'Id no valido'});
+    }
+    const encuesta = await Encuesta.findById(id_encuesta);
+    if (!encuesta){
+        return res.status(401).json({ success: false, message: 'La encuesta no existe'});
+    }
+    const pregunta = await Pregunta.findById(id_pregunta);
+    if (!pregunta){
+        return res.status(401).json({ success: false, message: 'La Pregunta no existe'});
+    }
+    let cant = 0;
+    var array = pregunta.optionRespuesta;
+    var array2 = array.map( (elem) =>
+        {
+           return {
+               id: elem.toString(),
+               nombre: '',
+               cant: 0
+           }
+        }
+
+    )
+    const aplicacions = await EncuestaAplicada.find({ id_encuesta: id_encuesta} );
+    aplicacions.map( (aplicacion) =>
+        aplicacion.answers.map( respuesta => 
+            // console.log('hola'),
+            (respuesta.id_question === id_pregunta ) &&
+                    array2.map( (elem)=>
+                        // console.log('hola'),
+                            respuesta.id_option_respuesta.map( (e) =>
+                                    // console.log(elem)
+                                    (elem.id === e) &&
+                                    (
+                                        elem.cant++
+                                        // console.log('a')
+                                    )
+                                
+                            )
+                        
+                    )  
+        )
+    )
+    
+    return res.json({ success: true, resultados: array2})
+
+}
 
 module.exports = {
     postEncuestaAplicada,
-    getEncuestasAplicadas
+    getEncuestasAplicadas,
+    getResultadosPr
 }
--- src/routes/encuestaAplicada.js
+++ src/routes/encuestaAplicada.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 const { check }  = require('express-validator');
-const { postEncuestaAplicada, getEncuestasAplicadas } = require('../controllers/EncuestaAplicadaController');
+const { postEncuestaAplicada, getEncuestasAplicadas, getResultadosPr } = require('../controllers/EncuestaAplicadaController');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 router.get('/', getEncuestasAplicadas);
+router.get('/resultados',[
+    check('id_encuesta', 'El campo id_encuesta es obligatorio').not().isEmpty(),
+    check('id_pregunta', 'El campo id_pregunta es obligatorio').not().isEmpty(),
+    validarCampos
+], getResultadosPr);
 router.post('/', [
     check('answers', 'El campo tipoPregunta es obligatorio').not().isEmpty(),
     validarCampos
